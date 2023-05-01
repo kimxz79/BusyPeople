5f09fa70014f3addfe9ad2cedc4c52c43d004437
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 00:11:20 +0900

    Update wordcloud.py

--- pages/wordcloud.py
+++ pages/wordcloud.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 font_url = 'https://raw.githubusercontent.com/seoinhyeok96/BusyPeople/main/font/NanumBarunGothic.ttf'
 
-wget.download(url, out='NanumBarunGothic.ttf')
+wget.download(url)
 
 df = pd.read_csv('https://raw.githubusercontent.com/seoinhyeok96/BusyPeople/main/data/%EC%96%B4%EA%B0%84.csv')
 
