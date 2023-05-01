c93516a3e121259b49087f916ed470f35c97e12f
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-02 01:49:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def get_tfidf_top_words(df, start_date=None, last_date=None, num_words=10, name=
     plot_bar(tfidf_top_words)
     plt.show()
 
-
+get_count_top_words(df, '2023-01-01', '2023-02-01', 50, '밴드')
 
 
 
