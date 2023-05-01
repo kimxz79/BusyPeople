4a739cdf06d5951d9ea0faf1647daaba7845572c
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 01:26:14 +0900

    Update wordcloud.py

--- pages/wordcloud.py
+++ pages/wordcloud.py
@@ -41,4 +41,9 @@ st.pyplot(fig)
 
 number = st.slider('Insert a number', min_value=1, step=1)
 bar_df = df.sort_values(by=['count'],ascending=False).reset_index(drop=True).iloc[:number]
-st.bar_chart(bar_df)
+
+st.write(bar_df)
+st.write(alt.Chart(bar_df).mark_bar().encode(
+    x=alt.X('index', sort=None),
+    y='count',
+))
