f392dcbb237bd9a328c53cc32161fcdddccf7c71
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-02 01:16:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('한눈에 보는 데이터 프레임')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
