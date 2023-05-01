0ce9e598a5de4b258923a411908d2d748350c352
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 01:13:19 +0900

    Update wordcloud.py

--- pages/wordcloud.py
+++ pages/wordcloud.py
@@ -40,5 +40,5 @@ plt.show()
 st.pyplot(fig)
 
 number = st.number_input('Insert a number', min_value=1 , step=1)
-bar_df = df.sort_values(by=['카운트'], ascending=False)
+bar_df = df.sort_values(by=['카운트'], ascending=False).reset_index(drop=True)
 st.bar_chart(bar_df)
