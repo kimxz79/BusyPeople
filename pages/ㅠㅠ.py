a1f5060ff3ff2df3f0ac3d865e29aad45dab9302
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-02 16:13:03 +0900

    Update ㅠㅠ.py

--- pages/ㅠㅠ.py
+++ pages/ㅠㅠ.py
@@ -18,22 +18,18 @@ options = st.multiselect(
 st.write('You selected:', options)
 
 
-# 파일 업로드를 위한 메시지를 출력합니다.
-st.write("CSV 파일을 업로드하세요.")
-
-# 파일 업로드를 받습니다.
-uploaded_file = st.file_uploader("")
-
-# 업로드된 파일이 있다면
-if uploaded_file is not None:
-    # 업로드된 파일을 읽어들입니다.
-    df = pd.read_csv(uploaded_file)
-
-    # 읽어들인 데이터를 출력합니다.
-    st.write(df)
+df = pd.read_csv('https://raw.githubusercontent.com/seoinhyeok96/BusyPeople/main/data/%ED%8A%B8%EB%A0%8C%EB%93%9C_%EC%A0%9C%EB%AA%A9%2B%EB%82%B4%EC%9A%A9.csv')
     
-    
-    
+ def plot_wordcloud(df,words):
+    wc = WordCloud(background_color="white", 
+                   max_words=1000,font_path = "AppleGothic", 
+                   contour_width=3, 
+                   colormap='Spectral', 
+                   contour_color='steelblue')
+    wc.generate_from_frequencies(words)
+    plt.figure(figsize=(10, 8))
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")   
     
 
 
