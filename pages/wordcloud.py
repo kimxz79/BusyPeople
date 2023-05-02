dced03da72e770e60f48dc74fbb24d05e693856e
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 16:05:52 +0900

    .

--- pages/wordcloud.py
+++ pages/wordcloud.py
@@ -56,8 +56,6 @@ colors = [plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)] for i in 
 weights = [random.randint(15, 35) for i in range(30)]
 
 data = go.Scatter(
-    x=[random.random() for i in range(200)],
-    y=[random.random() for i in range(200)],
     mode='text',
     text=words,
     textfont={
