772945a09256d52b92c87caeb4adb6751d1907f0
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-02 13:09:35 +0900

    Update ㅠㅠ.py

--- pages/ㅠㅠ.py
+++ pages/ㅠㅠ.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+from wordcloud import WordCloud
+import koreanize_matplotlib
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
+st.title('한눈에 보는 데이터 프레임')
+agree = st.checkbox('밴드')
+agree2 = st.checkbox('식물갤러리')
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/seoinhyeok96/BusyPeople/main/data/%ED%8A%B8%EB%A0%8C%EB%93%9C_%EC%A0%9C%EB%AA%A9%2B%EB%82%B4%EC%9A%A9.csv')
