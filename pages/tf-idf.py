6ce5c9221a22a76d4b8065ddac914e79e4884a87
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 10:23:20 +0900

    Update tf-idf.py

--- pages/tf-idf.py
+++ pages/tf-idf.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import koreanize_matplotlib
 
+from sklearn.feature_extraction.text import CountVectorizer
+
 from wordcloud import WordCloud
 
 
