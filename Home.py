30e7a6dc8dec34d31102f0976a8dc173b4dd2f24
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 13:38:00 +0900

    .

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title='바쁜사람들', layout="wide", initial_sidebar_state="collapsed")
 
-video_file = open('https://pixabay.com/videos/id-45961/', 'rb')
+video_file = open('https://youtu.be/n_QOv-nY_zM', 'rb')
 video_bytes = video_file.read()
 
 st.video(video_bytes)
