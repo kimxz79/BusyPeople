606fc80530009dd0455f38d05d134d9950c1007c
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 13:19:55 +0900

    .

--- Home.py
+++ Home.py
@@ -1,2 +1,3 @@
-st.set_page_config(page_title='바쁜사람들', layout="wide", initial_sidebar_state="collapsed")
+import streamlit as st
 
+st.set_page_config(page_title='바쁜사람들', layout="wide", initial_sidebar_state="collapsed")
