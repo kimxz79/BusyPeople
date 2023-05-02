85666fcc70772b00072075e761dab180e2f3c641
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-02 14:19:02 +0900

    Update 훈연습 중.py

--- pages/훈연습 중.py
+++ pages/훈연습 중.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-from streamlit_agraph import agraph, TripleStore
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def main():
     st.title('Networkx 그려보자')
@@ -9,28 +10,28 @@ def main():
     if st.button('분석하기'):
         st.success(f"{keyword}에 대한 연관어 분석 결과입니다")
 
+        # Define the data
+        data = [('식물', 'lemon', 8.773420979111814),
+                ('식물', '때싹', 8.166747080757542),
+                ('식물', '호잇', 8.006354458119853),
+                ('식물', '섬광탄', 7.954529050820706),
+                ('식물', '개굴', 7.7866337930292415),
+                ('식물', '빙고', 7.6275987515220764),
+                ('식물', '에스카르고', 7.4945584243029275)]
+
+        # Create the network graph
+        G = nx.DiGraph()
+        for row in data:
+            G.add_edge(row[0], row[1], weight=row[2])
+
+        pos = nx.spring_layout(G)
+
+        labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+
+        nx.draw(G, pos, with_labels=True)
+        plt.show()
+        st.pyplot()
+
 if __name__ == '__main__':
     main()
-
-# Define the data
-data = [('식물', 'lemon', 8.773420979111814),
-        ('식물', '때싹', 8.166747080757542),
-        ('식물', '호잇', 8.006354458119853),
-        ('식물', '섬광탄', 7.954529050820706),
-        ('식물', '개굴', 7.7866337930292415),
-        ('식물', '빙고', 7.6275987515220764),
-        ('식물', '에스카르고', 7.4945584243029275)]
-
-# Create a triple store from the data
-ts = TripleStore()
-for row in data:
-    ts.add_triple(*row)
-
-# Create the network graph
-st.title('Network Graph')
-graph = agraph(ts, font_bytes=font_bytes)
-st.write(graph)
-
-# Create the network graph
-st.title('Network Graph')
-agraph(ts)
