ec52412ddd3887437c2dc6a3e3e5c290115ae2d1
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 15:42:06 +0900

    .

--- pages/wordcloud.py
+++ pages/wordcloud.py
@@ -56,12 +56,12 @@ colors = [plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)] for i in 
 weights = [random.randint(15, 35) for i in range(30)]
 
 data = go.Scatter(
-    x=list(dict_0.values()) / 10,
-    y=list(dict_0.values()) / 10,
+    x=[i/10 for i in list(dict_0.values())],
+    y=[i/10 for i in list(dict_0.values())],
     mode='text',
     text=words,
     marker={'opacity': 0.3},
-    textfont={'size': list(dict_0.values()) / 10,
+    textfont={'size': [i/10 for i in list(dict_0.values())],
             'color': colors})
 
 layout = go.Layout({'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
