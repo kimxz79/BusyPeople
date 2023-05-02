ff1b15d7799b9f68a5d625988291bb0001d68ca0
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-02 11:36:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,49 +12,7 @@ agree = st.checkbox('밴드')
 agree2 = st.checkbox('식물갤러리')
 
 
-
-def plot_wordcloud(words):
-    wc = WordCloud(background_color="white", 
-                   max_words=1000,font_path = "AppleGothic", 
-                   contour_width=3, 
-                   colormap='Spectral', 
-                   contour_color='steelblue')
-    wc.generate_from_frequencies(words)
-    plt.figure(figsize=(10, 8))
-    plt.imshow(wc, interpolation='bilinear')
-    plt.axis("off")
-    st.pyplot()
-
-def plot_bar(words):
-    words_count = Counter(words)
-    words_df = pd.DataFrame.from_dict(words_count, orient='index', columns=['count'])
-    words_df.sort_values('count', ascending=False, inplace=True)
-    ax = words_df.plot(kind='bar', figsize=(10, 4))
-    ax.set_title('Top Words')
-    ax.set_xlabel('Words')
-    ax.set_ylabel('Count')
-    ax.tick_params(axis='x', labelrotation=45, labelsize=8)  
-    st.pyplot()
-
-def get_count_top_words(df, start_date=None, last_date=None, num_words=10, name=None):
-    if name is not None:
-        df = df[df['name'] == name]
-    if start_date is None:
-        start_date = df['time'].min().strftime('%Y-%m-%d')
-    if last_date is None:
-        last_date = df['time'].max().strftime('%Y-%m-%d')
-    df = df[(df['time'] >= start_date) & (df['time'] <= last_date)]
-    count_vectorizer = CountVectorizer()
-    count = count_vectorizer.fit_transform(df['title+content'].values)
-    count_df = pd.DataFrame(count.todense(), columns=count_vectorizer.get_feature_names_out())
-    count_top_words = count_df.sum().sort_values(ascending=False).head(num_words).to_dict()
-    
-    plot_wordcloud(count_top_words)
-    plot_bar(count_top_words)
-
-
-
-
+df = pd.read_csv('https://raw.githubusercontent.com/seoinhyeok96/BusyPeople/main/data/%ED%8A%B8%EB%A0%8C%EB%93%9C_%EC%A0%9C%EB%AA%A9%2B%EB%82%B4%EC%9A%A9.csv')
 
 
 
