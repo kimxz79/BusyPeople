4ca75382256f07793f4cb99ab3d307afa70ef466
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-02 21:51:59 +0900

    Update 👀 트렌드 알아보쟙.py

--- pages/👀 트렌드 알아보쟙.py
+++ pages/👀 트렌드 알아보쟙.py
@@ -19,7 +19,6 @@ rain(
 
 # 날짜 선택
 col1, col2, col3 = st.beta_columns(3)
-
 with col1:
     start_date = st.date_input("👉🏻시작 날짜",
                            value=datetime.today() - timedelta(days=45),
@@ -30,9 +29,12 @@ with col2:
                          value=datetime.today() - timedelta(days=30),    
                          min_value=datetime(2022, 4, 27),
                          max_value=datetime(2023, 4, 26))
-
 with col3:
     keyword_no = st.number_input("📌 키워드", value=50, min_value=1, step=1)
 
-st.write("🗓 ", start_date, "~", end_date)    
-st.write(number, '개의 키워드 선택')
+    
+col1, col2 = st.beta_columns(2)
+with col1:
+    st.write("🗓 ", start_date, "~", end_date)    
+with col2:
+    st.write(number, '개의 키워드 선택')
