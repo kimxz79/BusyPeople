264442971b0faaba4df88965758c333e231b0bb8
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 13:40:18 +0900

    .

--- Home.py
+++ Home.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title='바쁜사람들', layout="wide", initial_sidebar_state="collapsed")
 
-st.video('https://youtu.be/n_QOv-nY_zM')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.video('https://youtu.be/n_QOv-nY_zM')
+
+
