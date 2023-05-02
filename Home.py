5f3be11e6fdf19ffbc1317a9c6dab9edbc8d6a8e
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 13:18:04 +0900

    .

--- Home.py
+++ Home.py
@@ -1,1 +1,8 @@
-st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(
+    page_title='바쁜사람들', 
+    # page_icon=, 
+    layout="wide", 
+    initial_sidebar_state="collapsed", 
+    # menu_items=None
+    )
+
