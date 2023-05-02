60b01d5ae966eb130213562b85d0eca11fafda74
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-02 14:03:14 +0900

    .

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title='바쁜사람들', layout="wide", initial_sidebar_state="collapsed")
 
-col1, col2, col3, col4, col5, col6 = st.columns([2,1,1],[1,2,1])
+col1, col2, col3, col4, col5, col6 = st.columns([[2,1,1],[1,2,1]])
 
 with col4:
    st.header("식물병원이란?")
