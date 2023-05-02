3fc00697a1f715dd5450105ff7a81bd6c5c265a7
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-02 13:46:45 +0900

    Update ㅠㅠ.py

--- pages/ㅠㅠ.py
+++ pages/ㅠㅠ.py
@@ -51,5 +51,5 @@ def get_count_top_words(df, start_date=None, last_date=None, num_words=10, name=
     def load_data():
     # Load your data using pandas
     # Example:
-    df = pd.read_csv('https://raw.githubusercontent.com/seoinhyeok96/BusyPeople/main/data/%ED%8A%B8%EB%A0%8C%EB%93%9C_%EC%A0%9C%EB%AA%A9%2B%EB%82%B4%EC%9A%A9.csv')
+        df = pd.read_csv('https://raw.githubusercontent.com/seoinhyeok96/BusyPeople/main/data/%ED%8A%B8%EB%A0%8C%EB%93%9C_%EC%A0%9C%EB%AA%A9%2B%EB%82%B4%EC%9A%A9.csv')
     return df
